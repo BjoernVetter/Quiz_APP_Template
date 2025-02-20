21868f769dc21fedf35167a9e9bf6885931c416b
Bjoern Vetter <vetterbjoern@yahoo.de>
2025-02-20 08:48:55 +0100

    extra site commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// phew… not a lot going on here. Please add some code!
+console.clear();
+// Bookmark Button holen:
+const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
+
+// Answer Button holen:
+const answerButton = document.querySelector('[data-js="answerButton"]');
+
+// answer holen:
+const answer = document.querySelector('[data-js="answer"]');
+
+bookmarkButton.addEventListener("click", () => {
+  // console.log ("test");
+  // console.log (bookmarkButton.classList);
+  bookmarkButton.classList.toggle("bookmark--active");
+});
+
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer--active");
+
+  // Button_Text ändern
+
+  if (answer.classList.contains("card__answer--active")) {
+    answerButton.textContent = "Answer";
+  } else {
+    answerButton.textContent = "Show answer";
+  }
+});
